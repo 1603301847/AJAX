0fa0b19fdaece18631e507b54db19157f1d21a30
1603301847 <1903105953@qq.com>
2021-11-12 13:39:57 +0800

    jQuery发送AJAX请求

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 // 3.创建路由规则
 // request 是对请求报文的封装
 // response 是对响应报文的封装
-app.get('/server', (require, response) => {
+app.get('/server', (request, response) => {
     // 设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     // 设置响应体
     response.send('HELLO AJAX GET');
 })
 
-// app.post('/server', (require, response) => {
+// app.post('/server', (request, response) => {
 //     // 设置响应头 设置允许跨域
 //     response.setHeader('Access-Control-Allow-Origin', '*');
 //     // 设置响应体
@@ -22,7 +22,7 @@ app.get('/server', (require, response) => {
 // })
 
 // all可以接受任意类型的请求
-app.all('/server', (require, response) => {
+app.all('/server', (request, response) => {
     // 设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     // 设置自定义响应头
@@ -31,7 +31,7 @@ app.all('/server', (require, response) => {
     response.send('HELLO AJAX POAT');
 })
 
-app.all('/json-server', (require, response) => {
+app.all('/json-server', (request, response) => {
     // 设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     // 设置自定义响应头
@@ -50,7 +50,7 @@ app.all('/json-server', (require, response) => {
 })
 
 // 针对 IE 缓存
-app.all('/ie', (require, response) => {
+app.all('/ie', (request, response) => {
     // 设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -59,7 +59,7 @@ app.all('/ie', (require, response) => {
 })
 
 // 延时响应
-app.all('/delay', (require, response) => {
+app.all('/delay', (request, response) => {
     // 设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
     setTimeout(() => {
@@ -72,7 +72,12 @@ app.all('/delay', (require, response) => {
 app.all('/jquery-server', (request, response) => {
     // 设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
-    response.send('Hello jQuery AJAX');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+
+    const data = {
+        name: '尚硅谷'
+    };
+    response.send(JSON.stringify(data));
 })
 
 // 4.监听端口启动服务
